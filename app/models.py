68fb095a1bb9a162ef540ef072af08cead03c19b
Taras Faidiuk <taras.faidiuk@tangramltd.com>
2024-02-16 19:51:48 +0200

    feature: add to_json method to all models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,18 @@
-from sqlalchemy import ForeignKey
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    permissions = db.Column(db.String(256))  # NOTE: this will be stored like this `1,2,3,4,5, `
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'permissions': self.permissions.split(',')
+        }
 
 
 class User(db.Model):
@@ -19,32 +31,58 @@ class User(db.Model):
     def set_role(self, role_id):
         if Role.query.filter_by(id=role_id).first():
             self.role_id = role_id
-        raise Exception('No such role')
+        else:
+            raise Exception('Role Not Found')
+
+    def get_role(self) -> Role | None:
+        role = Role.query.filter_by(id=self.role_id).first()
+
+        if role:
+            return role
+        else:
+            return None
+
+    def is_admin(self):
+        role = Role.query.filter_by(name='Admin').first()
+
+        return self.role_id == role.id
 
     def to_json(self):
+        role = self.get_role()
+
+        if role:
+            role = role.to_json()
+        else:
+            role = None
+
         return {
             'id': self.id,
             'username': self.username,
             'email': self.email,
-            'role_id': self.role_id
+            'role': role
         }
 
     def __str__(self):
         return str(self.id) + ': ' + self.email
 
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    permissions = db.Column(db.String(256))  # NOTE: this will be stored like this `1,2,3,4,5, `
-
-
 class Permission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True, unique=True)
 
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 
 class Salary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     salary = db.Column(db.Float)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'salary': self.salary
+        }
