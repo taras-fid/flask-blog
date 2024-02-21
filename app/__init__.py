49551cd11225a40551631500d108140c63d72575
Taras Faidiuk <taras.faidiuk@tangramltd.com>
2024-02-21 20:48:42 +0200

    feature!: add flask-mail service to app
    add config values to your local config.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
-from config import Config
+
+import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import timedelta
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(config.Config)
+app.config['MAIL_SERVER'] = config.MAIL_SERVER
+app.config['MAIL_PORT'] = config.MAIL_PORT
+app.config['MAIL_USERNAME'] = config.MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = config.MAIL_PASSWORD
+app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
+app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
 app.permanent_session_lifetime = timedelta(minutes=90)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app import routes
 from app import models
